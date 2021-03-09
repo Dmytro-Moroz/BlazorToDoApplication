0198fd0b0f02c26e18252eac80e08125fa2cece6
dmoroz <avangardi@ukr.net>
2021-03-09 16:29:40 +0200

    com1

---
+++ BlazorSamples.ToDoGeneral/Data/ToDoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSamples.ToDoGeneral.Data
+{
+    public class ToDoModel
+    {
+        public string TaskName { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
